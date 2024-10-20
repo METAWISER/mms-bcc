9d55b15c73f8439a4329764a381774307797b545
Carlos <metawiser@gmail.com>
2024-10-20 13:31:24 +0200

    feat: Adds bcript value object

---
+++ src/domain/value-objects/bcrypt.vo.ts
@@ -0,0 +1,23 @@
+import * as bcrypt from 'bcrypt';
+
+export class Bcrypt {
+  readonly value: string;
+
+  constructor(value: string) {
+    this.value = value;
+  }
+
+  static async create(rawPassword: string): Promise<Bcrypt> {
+    const hashedPassword = await bcrypt.hash(rawPassword, 10);
+
+    return new Bcrypt(hashedPassword);
+  }
+
+  async compare(rawPassword: string): Promise<boolean> {
+    return bcrypt.compare(rawPassword, this.value);
+  }
+
+  toPrimitives(): string {
+    return this.value;
+  }
+}
