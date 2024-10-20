21e8b56f517c3a26045072a7c1e7786d849d4c4b
Carlos <metawiser@gmail.com>
2024-10-20 23:59:27 +0200

    feat: Adds auth resolver methods

---
+++ src/auth/auth.resolver.ts
@@ -0,0 +1,33 @@
+import { UseGuards } from '@nestjs/common';
+import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
+import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { SignupInput, LoginInput } from './dto/inputs';
+import { AuthResponse } from './types/auth-response.type';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { User } from 'src/users/entities/user.entity';
+
+@Resolver()
+export class AuthResolver {
+  constructor(private readonly authService: AuthService) {}
+
+  @Mutation(() => AuthResponse, { name: 'signup' })
+  async signUp(
+    @Args('signupInput') signupInput: SignupInput,
+  ): Promise<AuthResponse> {
+    return this.authService.signUp(signupInput);
+  }
+
+  @Mutation(() => AuthResponse, { name: 'login' })
+  async login(
+    @Args('loginInput') loginInput: LoginInput,
+  ): Promise<AuthResponse> {
+    return this.authService.login(loginInput);
+  }
+
+  @Query(() => AuthResponse, { name: 'revalidate' })
+  @UseGuards(JwtAuthGuard)
+  revalidateToken(@CurrentUser() user: User): AuthResponse {
+    return this.authService.revalidateToken(user);
+  }
+}
