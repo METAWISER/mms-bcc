5caf2bb5c5e8242a17b7bfe64447a00ac17b6c91
Carlos <metawiser@gmail.com>
2024-10-20 23:55:08 +0200

    feat: Adds all app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,43 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AppConfigModule } from './config/app/app-config.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { UsersModule } from './users/users.module';
+import { OrdersModule } from './orders/orders.module';
+import { ProductsModule } from './products/products.module';
+import { WorkersModule } from './workers/workers.module';
+import { AuthModule } from './auth/auth.module';
+import { DomainModule } from './domain/domain.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI, {}),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
+      csrfPrevention: false,
+      sortSchema: true,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      includeStacktraceInErrorResponses: false,
+    }),
+    AppConfigModule,
+    UsersModule,
+    OrdersModule,
+    ProductsModule,
+    WorkersModule,
+    AuthModule,
+    DomainModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
