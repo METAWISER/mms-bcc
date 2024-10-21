d5a73662f116a4ee2e8594964d76aad8391e6460
Carlos <metawiser@gmail.com>
2024-10-21 11:17:40 +0200

    adjustment: Blockquote createUser mutation - Deprecated do to signup mutation in auth resolver

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,16 +1,19 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
-import { CreateUserInput } from './dto/inputs/create-user.input';
+// !* Deprecated do to signup mutation in auth.resolver.ts
+//import { CreateUserInput } from './dto/inputs/create-user.input';
 import { GetUserArgs } from './dto/args/get-user.arg';
+import { ValidRolesArgs } from './dto/args/valid-roles.arg';
 
 @Resolver(() => User)
 export class UserResolver {
   constructor(private readonly userService: UsersService) {}
 
   @Query(() => [User], { name: 'users', description: 'Get all users' })
-  async findAll(): Promise<User[]> {
-    return await this.userService.findAll();
+  async findAll(@Args() validRoles: ValidRolesArgs): Promise<User[]> {
+    console.log(validRoles.roles);
+    return await this.userService.findAll(validRoles.roles);
   }
 
   @Query(() => User, { name: 'user', description: 'Get user by id' })
@@ -20,7 +23,8 @@ export class UserResolver {
     return await this.userService.findOne(getUserArgs.id);
   }
 
-  @Mutation(() => User, {
+  // ! Deprecated do to signup mutation in auth.resolver.ts
+  /*   @Mutation(() => User, {
     name: 'createUser',
     description: 'Create a user',
   })
@@ -28,7 +32,7 @@ export class UserResolver {
     @Args('createUserInput') createUserInput: CreateUserInput,
   ): Promise<User | null> {
     return await this.userService.create(createUserInput);
-  }
+  } */
 
   @Mutation(() => User, {
     name: 'deleteUser',
