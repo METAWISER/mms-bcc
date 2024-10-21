a72027c05fea7483522785c9c2ea44a19cd45e39
Carlos <metawiser@gmail.com>
2024-10-22 00:25:56 +0200

    feat: Adds orders resolver with createOrder, assignEmployee and findAll query mutations

---
+++ src/orders/orders.resolver.ts
@@ -0,0 +1,48 @@
+import { UseGuards } from '@nestjs/common';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { OrdersService } from './orders.service';
+import { Order } from './entities/order.entity';
+import { CreateOrderInput } from './dto/inputs/create-order.input';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { ValidRoles } from 'src/auth/enums/valid-roles.enum';
+import { User } from 'src/users/entities/user.entity';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { GetOrdersArgs } from './dto/args/get-orders.arg';
+import { AssignEmployeeInput } from './dto/inputs/assign-employee.input';
+
+@Resolver(() => Order)
+@UseGuards(JwtAuthGuard)
+export class OrdersResolver {
+  constructor(private readonly ordersService: OrdersService) {}
+
+  @Query(() => [Order], { name: 'orders' })
+  findAll(
+    @Args() getOrdersArgs: GetOrdersArgs,
+    @CurrentUser([ValidRoles.ADMIN, ValidRoles.EMPLOYEE]) user: User,
+  ) {
+    void user;
+    return this.ordersService.findAll(getOrdersArgs);
+  }
+
+  @Mutation(() => Order, {
+    name: 'createOrder',
+    description: 'Create an order, only available for clients',
+  })
+  async createOrder(
+    @Args('createOrderInput') createOrderInput: CreateOrderInput,
+    @CurrentUser([ValidRoles.CLIENT]) user: User,
+  ): Promise<Order> {
+    return this.ordersService.create(createOrderInput, user);
+  }
+
+  @Mutation(() => Order, {
+    name: 'assignEmployee',
+    description: 'Assign an employee to an order',
+  })
+  async assignEmployee(
+    @Args('assignEmployeeInput') assignEmployeeInput: AssignEmployeeInput,
+    @CurrentUser([ValidRoles.ADMIN, ValidRoles.EMPLOYEE]) user: User,
+  ): Promise<Order> {
+    return await this.ordersService.assignEmployee(assignEmployeeInput, user);
+  }
+}
