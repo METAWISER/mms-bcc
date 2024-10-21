b074054383cda082294f3a385f62718e44656e72
Carlos <metawiser@gmail.com>
2024-10-22 00:18:03 +0200

    feat: Adds orders module

---
+++ src/orders/orders.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { OrdersResolver } from './orders.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order, OrderSchema } from './entities/order.entity';
+import { ProductsModule } from '../products/products.module';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
+    ProductsModule,
+  ],
+  providers: [OrdersResolver, OrdersService],
+})
+export class OrdersModule {}
